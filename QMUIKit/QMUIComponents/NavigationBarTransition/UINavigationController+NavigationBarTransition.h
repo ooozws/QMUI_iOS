485b44a2983b3aa34836d76f53b1d6c1e7359d64
ooozws <weisaizhang@gmail.com>
2020-10-24 01:26:20 +0800

    fixed

--- QMUIKit/QMUIComponents/NavigationBarTransition/UINavigationController+NavigationBarTransition.h
+++ QMUIKit/QMUIComponents/NavigationBarTransition/UINavigationController+NavigationBarTransition.h
@@ -16,22 +16,9 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-@interface _QMUITransitionNavigationBar : UINavigationBar
-
-@end
-
 /**
  *  因为系统的UINavigationController只有一个navBar，所以会导致在切换controller的时候，如果两个controller的navBar状态不一致（包括backgroundImgae、shadowImage、barTintColor等等），就会导致在刚要切换的瞬间，navBar的状态都立马变成下一个controller所设置的样式了，为了解决这种情况，QMUI给出了一个方案，有四个方法可以决定你在转场的时候要不要使用自定义的navBar来模仿真实的navBar。
  */
 @interface UINavigationController (NavigationBarTransition)
-
-@property(nonatomic, strong) _QMUITransitionNavigationBar *transitionNavigationBar;
-
-@end
-
-@interface UIViewController (NavigationBarTransition)
-
-/// 用来模仿真的navBar的，在转场过程中存在的一条假navBar
-@property(nonatomic, strong) _QMUITransitionNavigationBar *transitionNavigationBar;
 
 @end
